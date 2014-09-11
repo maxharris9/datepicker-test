0b4ff1e69e6d7a5ccafa7ecec99e5ac462dbf96c
Max Harris <harris.max@gmail.com>
2014-09-11 16:40:30 -0700

    Updated date picker demo: added demo collection, renamed div id

--- datepicker.js
+++ datepicker.js
@@ -1,8 +1,15 @@
+Dates = new Mongo.Collection("dates");
+
 if (Meteor.isClient) {
 	Template.hello.helpers({
 		rendered: function () {
 			for (var i = 0; i < 10; i++) {
-				Template.datePicker.setup('12/02/1980', function (date) { console.log('saved, with this date:', date); } );
+				Template.datePicker.setup('12/2/1980', function (date) {
+					Dates.insert( { 'newDate': date } );
+					console.log('saved, with this date:', date);
+				},
+
+				'editableDateControl' );
 			}
 		}
 	});
