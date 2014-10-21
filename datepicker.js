f03209181539919754a9db02a08832563dfae717
Chris Alexander <chris.alexander@mac.com>
2014-10-21 12:08:30 -0700

    Changed JS to updated meteor syntax for 0.9.4

--- datepicker.js
+++ datepicker.js
@@ -1,19 +1,17 @@
 Dates = new Mongo.Collection("dates");
 
 if (Meteor.isClient) {
-	Template.hello.helpers({
-		rendered: function () {
-			for (var i = 0; i < 10; i++) {
-				Template.datePicker.setup('12/2/1980', function (date) {
-					Dates.insert( { 'newDate': date } );
-					console.log('saved, with this date:', date);
-				}, 'editableDateControl', 12, 32);
-			}
-
-			Template.textField.setup('',
-				function (text) {
-					console.log('saved, with this text:', text);
-				}, 'textFieldControl');
+	Template.hello.rendered = function () {
+		for (var i = 0; i < 10; i++) {
+			Template.datePicker.setup('12/2/1980', function (date) {
+				Dates.insert( { 'newDate': date } );
+				console.log('saved, with this date:', date);
+			}, 'editableDateControl', 12, 32);
 		}
-	});
+
+		Template.textField.setup('',
+			function (text) {
+				console.log('saved, with this text:', text);
+			}, 'textFieldControl');
+	};
 }
